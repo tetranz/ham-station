cda8333a3524e01cdf09ee2b16524665c9908622
Ross Keatinge <tetranz@gmail.com>
2018-03-18 13:59:00 -0400

    Display address without country.

---
+++ src/Plugin/Field/FieldFormatter/AddressNoCountryFormatter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\ham_station\Plugin\Field\FieldFormatter;
+
+use Drupal\address\Plugin\Field\FieldFormatter\AddressDefaultFormatter;
+use Drupal\Core\Render\Element;
+
+/**
+ * Plugin implementation of the 'address_no_country' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "address_no_country",
+ *   label = @Translation("No country"),
+ *   field_types = {
+ *     "address",
+ *   },
+ * )
+ */
+class AddressNoCountryFormatter extends AddressDefaultFormatter {
+
+  public static function postRender($content, array $element) {
+    /** @var \CommerceGuys\Addressing\AddressFormat\AddressFormat $address_format */
+    $address_format = $element['address_format']['#value'];
+    $format_string = $address_format->getFormat();
+
+    $replacements = [];
+    foreach (Element::getVisibleChildren($element) as $key) {
+      $child = $element[$key];
+      if (isset($child['#placeholder'])) {
+        $replacements[$child['#placeholder']] = $child['#value'] ? $child['#markup'] : '';
+      }
+    }
+    $content = self::replacePlaceholders($format_string, $replacements);
+    $content = nl2br($content, FALSE);
+
+    return $content;
+  }
+
+}
