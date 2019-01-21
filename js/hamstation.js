2282837ffce3396417144a679d7150e491d8758c
Ross Keatinge <tetranz@gmail.com>
2019-01-21 16:31:17 -0500

    Change zoom.

--- js/hamstation.js
+++ js/hamstation.js
@@ -34,7 +34,7 @@ const hamstationApp = (function ($) {
       let map_container = document.querySelector('.map-container');
 
       map = new google.maps.Map(map_container, {
-        zoom: 14
+        zoom: 13
       });
 
       map.addListener('center_changed', function () {
